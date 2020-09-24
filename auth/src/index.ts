87a0c243d72076e292ae6ea04265fcb8fc150620
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-24 13:33:48 -0700

    installed express-async-errors so can add async keyword to routes without adding next parameter

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,8 @@
 import express from "express";
+// if throw an error express automatically sends to error handling middleware
+// if have an async route need the request to receive the next function and need next(new Error)instead of throw new error
+// solution install express-async-errors
+import "express-async-errors";
 import { json } from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -13,8 +17,8 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
-// for routes that don't exist for any http verb 
-app.all("*", () => {
+// for routes that don't exist for any http verb
+app.all("*", async () => {
 	throw new NotFoundError();
 });
 app.use(errorHandler);
