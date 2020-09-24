0be20c2e1b822124bdd23f4e086d7c2ee44756d9
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-24 15:35:25 -0700

    badrequestError created to extend custom Error

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/User";
+import { BadRequestError } from "../errors/bad-request-error";
 const router = express.Router();
 
 router.post(
@@ -22,8 +23,7 @@ router.post(
 		const existingUser = await User.findById({ email });
 
 		if (existingUser) {
-			console.log("email in use");
-			return res.send({});
+			throw new BadRequestError("Email in Use");
 		}
 
 		// create user
