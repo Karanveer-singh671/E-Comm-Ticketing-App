83a8d85c2325efe2d0df0c82ef21fee547dc5ee0
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-25 13:28:28 -0700

    middleware added for requestValidation to throw error if not valid. Made reusable since will be calling in most route handlers so don't want to duplicate the throw error logic

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,18 +1,19 @@
-import { Request, Response, NextFunction } from 'express';
-import { CustomError } from '../errors/custom-error';
-
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../errors/custom-error";
+// express distinguishes between an error handling middleware and normal one by the
+// number of arguments that the function accepts
 export const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
+	err: Error,
+	req: Request,
+	res: Response,
+	next: NextFunction
 ) => {
-  if (err instanceof CustomError) {
-    console.log(err);
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-  }
+	if (err instanceof CustomError) {
+		console.log(err);
+		return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+	}
 
-  res.status(400).send({
-    errors: [{ message: 'Something went wrong' }]
-  });
+	res.status(400).send({
+		errors: [{ message: "Something went wrong" }],
+	});
 };
