ca05580ff434f5a98aea351062021d8141635b56
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-25 14:20:56 -0700

    added current User middleware to abstract logic out of route handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,24 +1,16 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
 // used to see if user logged in since React app will not be able to look at the cookie and check if has a valid JWT
-router.get("/api/users/currentUser", (req, res) => {
-	// if session or jwt does not exist then the user is not logged in
-	// '!req.session || req.session.jwt' === to '!req.session?.jwt'
-	// '?' used to see if property exists or not
-	if (!req.session?.jwt) {
-		return res.send({ currentUser: null });
-	}
-	// verify the signature of the JWT to make sure that the jwt has not been modified by the user in any way
-	// if the jwt has not been modified by the user, the decoded payload
-	try {
-		const payload = jwt.verify(req.session!.jwt, process.env.JWT_KEY!);
-		res.send({ currentUser: payload });
-	} catch (err) {
-		res.send({ currentUser: null });
-	}
+router.get("/api/users/currentUser", currentUser, (req, res) => {
+	// abstracted information to middleware
+	res.send({
+		// set as current user or null so won't default to undefined
+		currentUser: req.currentUser || null,
+	});
 });
 
 export { router as currentUserRouter };
