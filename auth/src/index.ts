5bda3ab9972e10d06d5e31bf26b2e27b741d1d43
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-25 11:42:26 -0700

    created cookie session to store JWT, added trust proxy true since using ingress-nginx as a proxy

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,44 +1,54 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import mongoose from 'mongoose';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+// traffic is being proxy'd to our app thru ingress-nginx
+// makes express aware of this and trust this proxy since default is not to trust
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+	cookieSession({
+		signed: false, // no encryption
+		secure: true, // Https connection
+	})
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
+app.all("*", async (req, res) => {
+	throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
 const start = async () => {
-  try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    });
-    console.log('Connected to MongoDb');
-  } catch (err) {
-    console.error(err);
-  }
+	try {
+		await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log("Connected to MongoDb");
+	} catch (err) {
+		console.error(err);
+	}
 
-  app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
-  });
+	app.listen(3000, () => {
+		console.log("Listening on port 3000!!!!!!!!");
+	});
 };
 
 start();
