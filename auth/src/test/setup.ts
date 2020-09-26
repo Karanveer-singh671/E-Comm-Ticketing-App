41a2f344acdf3c52b74c81254a7929d2ce2a37a3
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-26 10:40:35 -0700

    setup file created for testing with beforeAll, beforeEach, afterAll hooks

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,31 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose, { mongo } from "mongoose";
+import { app } from "../app";
+let mongo: any;
+// before any tests start up create MongoMemoryServer instance in memory
+// allow us to run multiple different test suites at same time across different micro-services
+// w/ out them trying to reach to same instance of MongoDB
+// define a hook function here that will run before all our tests
+beforeAll(async () => {
+	mongo = new MongoMemoryServer();
+	const mongoUri = await mongo.getUri();
+
+	// connect mongoose to in memory MongoMemoryServer
+	await mongoose.connect(mongoUri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+});
+// before each test is run hook, reset all data before each test
+beforeEach(async () => {
+	const collections = await mongoose.connection.db.collections();
+	for (let collection of collections) {
+		// delete all documents in a collection
+		await collection.deleteMany({});
+	}
+});
+// disconnect from mongo server instance after all tests complete
+afterAll(async () => {
+	await mongo.stop();
+	await mongoose.connection.close();
+});
