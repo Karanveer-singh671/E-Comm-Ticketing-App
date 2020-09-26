8b7cf1e5b3fb96997dd766ff1c949505fb44e296
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-26 11:09:56 -0700

    basic 400 tests created

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
-
+// return or await request 
 it("should return a 201 on a successful signup", async () => {
 	return request(app)
 		.post("/api/users/signup")
@@ -10,4 +10,48 @@ it("should return a 201 on a successful signup", async () => {
 			password: "password",
 		})
 		.expect(201);
+});
+
+it("should return a 400 with an invalid email", async () => {
+	return request(app)
+		.post("/api/users/signup")
+		.send({
+			// body of request
+			email: "acom",
+			password: "password",
+		})
+		.expect(400);
+});
+
+it("should return a 400 with an invalid password", async () => {
+	return request(app)
+		.post("/api/users/signup")
+		.send({
+			// body of request
+			email: "a@a.com",
+			password: "1",
+		})
+		.expect(400);
+});
+
+it("should return a 400 with missing email and/or password", async () => {
+  await request(app)
+  .post("/api/users/signup")
+  .send({
+    // body of request
+    email: "a@a.com",
+  })
+  await request(app)
+  .post("/api/users/signup")
+  .send({
+    // body of request
+    password: "12345",
+  })
+  .expect(400);
+  await request(app)
+		.post("/api/users/signup")
+		.send({
+			// body of request
+		})
+		.expect(400);
 });
