c862f94b1c7a5a7f4aebe39bc2a95b836a472dc4
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-27 13:16:03 -0700

    errors displaying on frontend, need to create a helper since a lot of the logic will be duplicated for other pages

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,13 +1,22 @@
 // now path nextJS uses will be auth/signup
 import { useState } from "react";
+import axios from "axios";
 
 export default () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-
-	const onSubmit = (e) => {
+	const [errors, setErrors] = useState([]);
+	const onSubmit = async (e) => {
 		e.preventDefault();
-		console.log(email, password);
+		try {
+			const response = await axios.post("/api/users/signup", {
+				email,
+				password,
+			});
+			console.log(response.data);
+		} catch (err) {
+			setErrors(err.response.data.errors);
+		}
 	};
 
 	return (
@@ -33,6 +42,16 @@ export default () => {
 					/>
 				</div>
 			</div>
+			{errors.length > 0 && (
+				<div className="alert alert-danger">
+					<h4>Ooops....</h4>
+					<ul className="my-0">
+						{errors.map((err) => (
+							<li key={err.message}>{err.message}</li>
+						))}
+					</ul>
+				</div>
+			)}
 			<button className="btn btn-primary">Sign Up</button>
 		</form>
 	);
