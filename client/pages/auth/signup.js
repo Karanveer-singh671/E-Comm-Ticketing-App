2500f20fea838f54d0c99f528fe77b516a38a024
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-27 12:58:10 -0700

    basic sign up form in auth/signup route

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,3 +1,39 @@
 // now path nextJS uses will be auth/signup
+import { useState } from "react";
 
+export default () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log(email, password);
+	};
+
+	return (
+		<form onSubmit={onSubmit}>
+			<h1>Sign Up</h1>
+			<div className="form-group">
+				<label>Email Address</label>
+				<input
+					type="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					className="form-control"
+					placeholder="Enter Email..."
+				/>
+				<div className="form-group">
+					<label>Password</label>
+					<input
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						className="form-control"
+						placeholder="Enter Password..."
+					/>
+				</div>
+			</div>
+			<button className="btn btn-primary">Sign Up</button>
+		</form>
+	);
+};
