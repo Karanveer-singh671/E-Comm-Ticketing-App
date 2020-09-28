da2579c43a021c239fc6d8bb218773484fb2b7eb
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-28 12:49:20 -0700

    reusable api client for determining whether request comes from browser or not as well as creating the baseURL for the request and headers

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,29 +1,15 @@
 import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-	console.log("LandingPage -> I am in the component", currentUser);
 	return <h1>Landing Page</h1>;
 };
 // get initial props is our chance to get data that we need in our application during SSR process! e.g current User
 // when we try to receive this component this method is called first
-LandingPage.getInitialProps = async () => {
-	if (typeof window === "undefined") {
-		// we are on the server so requests should be made to http://ingress-nginx.ingress-nginx.svc.cluster.local
-		const response = await axios.get(
-			"http://ingress-nginx-controler.ingress-nginx.svc.cluster.local//api/users/currentuser",
-			{
-				headers: {
-					Host: "ticketing.dev",
-				},
-			}
-		);
-		return response.data;
-	} else {
-		// on the browser since window exists requests can be made w/ base url of ''
-		const response = await axios.get("/api/users/currentuser");
-		return response.data;
-	}
-	return {};
+LandingPage.getInitialProps = async (context) => {
+	const client = await buildClient(context);
+	const { data } = await client.get("/api/users/currentuser");
+	return data;
 };
 
 export default LandingPage;
