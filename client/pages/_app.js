2b0771145a2bc5e9ffb00553d1e45f458720e4c8
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-28 13:27:05 -0700

    basic header component and called in _app passed props to it from App

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,10 +2,11 @@ import "bootstrap/dist/css/bootstrap.css";
 // global css added in _app file since this file will be loaded every time a user comes to application
 // if do css in any other .js file in pages dir won't show so need to make custom wrapper to include the css
 import buildClient from "../api/build-client";
+import Header from "../components/header";
 const AppComponent = ({ Component, pageProps, currentUser }) => {
 	return;
 	<div>
-		<h1>Header for each page</h1>
+		<Header currentUser={currentUser}/>
 		<Component {...pageProps} />
 	</div>;
 };
@@ -17,17 +18,17 @@ AppComponent.getInitialProps = async (appContext) => {
 	const client = buildClient(appContext.ctx);
 	const { data } = await client.get("/api/users/currentuser");
 	// call getInitialProps for individual page since when add in appComponent get Initial props the page
-  // get InitialProps are no longer invoked, do a check to make sure the page has getInitialProps since 
-  // some pages do not have getInitialProps
+	// get InitialProps are no longer invoked, do a check to make sure the page has getInitialProps since
+	// some pages do not have getInitialProps
 	let pageProps;
 	if (appContext.Component.getInitialProps) {
 		const pageProps = await appContext.Component.getInitialProps(
 			appContext.ctx
 		);
-  }
-  return {
-    pageProps,
-    currentUser: data.currentUser
-  }
+	}
+	return {
+		pageProps,
+		currentUser: data.currentUser,
+	};
 };
 export default AppComponent;
