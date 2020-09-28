96d95bfac0f61592ec400ce59dc156499108a98d
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-28 10:27:28 -0700

    making getInitialProps function and use an axios request to fetch currentUser. Note: can only use hooks inside component but a component cannot fetch data on the intial loading on a server side rendering app therefore need to use the getInitialProps function

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,15 @@
-const LandingPage = ({ color }) => {
-  console.log("LandingPage -> I am in the component", color)
+import axios from "axios";
+
+const LandingPage = ({ curretnUser }) => {
+	console.log("LandingPage -> I am in the component", curretnUser);
 	return <h1>Landing Page</h1>;
 };
 // get initial props is our chance to get data that we need in our application during SSR process! e.g current User
 // when we try to receive this component this method is called first
-LandingPage.getInitialProps = () => {
-	"I am on";
-	console.log("I am on the server");
+LandingPage.getInitialProps = async () => {
+	const response = await axios.get("/api/users/currentuser");
 
-	return { color: "red" };
+	return response.data;
 };
 
 export default LandingPage;
