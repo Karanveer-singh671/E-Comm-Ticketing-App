5b19b92484cc1914db062506f243c6a23999b401
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-28 11:46:29 -0700

    cross namespace service communication added, and code to specify if making request from browser or not

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,9 +7,23 @@ const LandingPage = ({ currentUser }) => {
 // get initial props is our chance to get data that we need in our application during SSR process! e.g current User
 // when we try to receive this component this method is called first
 LandingPage.getInitialProps = async () => {
-	const response = await axios.get("/api/users/currentuser");
-
-	return response.data;
+	if (typeof window === "undefined") {
+		// we are on the server so requests should be made to http://ingress-nginx.ingress-nginx.svc.cluster.local
+		const response = await axios.get(
+			"http://ingress-nginx-controler.ingress-nginx.svc.cluster.local//api/users/currentuser",
+			{
+				headers: {
+					Host: "ticketing.dev",
+				},
+			}
+		);
+		return response.data;
+	} else {
+		// on the browser since window exists requests can be made w/ base url of ''
+		const response = await axios.get("/api/users/currentuser");
+		return response.data;
+	}
+	return {};
 };
 
 export default LandingPage;
