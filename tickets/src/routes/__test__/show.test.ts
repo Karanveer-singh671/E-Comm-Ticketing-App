ce01e96e8e8acaec9f1cb7318f760707338dd377
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-30 11:55:37 -0700

    made mofication to common module, added test for fetch list of tickets

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,11 +1,11 @@
 import request from "supertest";
 import { app } from "../../app";
+import mongoose from "mongoose";
 
 it("returns a 404 if the ticket is not found", async () => {
-	const response = await request(app)
-		.post("/api/tickets/sdjdsfjdfjndfs")
-		.send()
-		.expect(404);
+	// get a valid object id from mongoose
+	const id = new mongoose.Types.ObjectId().toHexString();
+	await request(app).post(`/api/tickets/${id}`).send().expect(404);
 });
 
 it("returns the ticket if the ticket is found", async () => {
