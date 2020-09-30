54e9dcd87686fa94c4e447df27377e5ffcbb0fe8
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-09-30 11:22:15 -0700

    added tests for get ticket route handler

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,28 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns a 404 if the ticket is not found", async () => {
+	await request(app).post("/api/tickets/sdjdsfjdfjndfs").send().expect(404);
+});
+
+it("returns the ticket if the ticket is found", async () => {
+	const title = "concert";
+	const price = 20;
+	// response contains entire ticket since sent in route handler
+	const response = await request(app)
+		.post("/api/tickets")
+		.set("Cookie", global.signin())
+		.send({
+			title,
+			price,
+		})
+		.expect(201);
+	// since ticket was created should be able to return the ticket
+	const ticketResponse = await request(app)
+		.get(`/api/tickets/${response.body.id}`)
+		.send()
+		.expect(200);
+
+	expect(ticketResponse.body.title).toEqual(title);
+	expect(ticketResponse.body.price).toEqual(price);
+});
