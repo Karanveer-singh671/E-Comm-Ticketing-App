d3d97cfbf1665c8966c6d8a9104e5dda51eecbdb
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-10-01 13:54:34 -0700

    created abstract class Listener to correctly create and check new listeners that are created within the application

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
 
 console.clear();
@@ -11,17 +11,20 @@ stan.on("connect", () => {
 
 	stan.on("close", () => {
 		console.log("NATS connection closed!");
-    // close client
+		// close client
 		process.exit();
 	});
 	// set setManualAckMode to true then won't lose event until set event passed to true
-  // get history of events setDeliverAllAvailable() if service goes down but if list of events super large
-  // -> durable subscription to see if a service has processed that event and NATS will store what events a service has processed and what missed out on
-  // when service comes back online with same client ID it will send what need to process (instead of process all events over again)
-  // still need setDeliverAllAvailable since bring a service online for the first time can get all events emitted in the past
-  // then when restart service will only send unprocessed events from there 
-	const options = stan.subscriptionOptions().setDeliverAllAvailable().setDurableName('some-service')
-  .setManualAckMode(true);
+	// get history of events setDeliverAllAvailable() if service goes down but if list of events super large
+	// -> durable subscription to see if a service has processed that event and NATS will store what events a service has processed and what missed out on
+	// when service comes back online with same client ID it will send what need to process (instead of process all events over again)
+	// still need setDeliverAllAvailable since bring a service online for the first time can get all events emitted in the past
+	// then when restart service will only send unprocessed events from there
+	const options = stan
+		.subscriptionOptions()
+		.setDeliverAllAvailable()
+		.setDurableName("some-service")
+		.setManualAckMode(true);
 	const subscription = stan.subscribe(
 		"ticket:created",
 		"listenerQueueGroup", // makes sure even if we disconnect will not dump away durable service storage and only send event to one service
@@ -39,7 +42,48 @@ stan.on("connect", () => {
 		msg.ack();
 	});
 });
-// when restart a listener call close 
+// when restart a listener call close
 process.on("SIGINT", () => stan.close());
 
 process.on("SIGTERM", () => stan.close());
+
+// every listerer will need what is inside the listener abstract class
+abstract class Listener {
+	abstract onMessage(data: any, msg: Message): void;
+	abstract subject: string;
+	abstract queueGroupName: string;
+	private client: Stan;
+	protected ackWait = 5 * 1000;
+
+	constructor(client: Stan) {
+		this.client = client;
+	}
+	subscriptionOptions() {
+		return this.client
+			.subscriptionOptions()
+			.setDeliverAllAvailable()
+			.setManualAckMode(true)
+			.setAckWait(this.ackWait)
+			.setDurableName(this.queueGroupName);
+	}
+	listen() {
+		const subscription = this.client.subscribe(
+			this.subject,
+			this.queueGroupName,
+			this.subscriptionOptions()
+		);
+		subscription.on("message", (msg: Message) => {
+			console.log(`Message Received: ${this.subject} / ${this.queueGroupName}`);
+
+			const parsedData = this.parseMessage(msg);
+			this.onMessage(parsedData, msg);
+		});
+	}
+	parseMessage(msg: Message) {
+		const data = msg.getData();
+		return typeof data === "string"
+			? JSON.parse(data)
+			: // how to get data from a buffer
+			  JSON.parse(data.toString("utf8"));
+	}
+}
