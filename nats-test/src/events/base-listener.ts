9a9071e712ab72ffcd738080930960e6a22904d0
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-10-02 09:59:43 -0700

    added generic for class so will be of whatever type argument provided, enforced listener subjects

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -1,7 +1,16 @@
 import { Message, Stan } from "node-nats-streaming";
-export abstract class Listener {
-	abstract onMessage(data: any, msg: Message): void;
-	abstract subject: string;
+import { Subjects } from "./subjects";
+
+interface Event {
+	subject: Subjects;
+	data: any;
+}
+// set up listener as generic class so when try to extend listener need to provide custom type to it!
+// 'T' is like an argument for types
+export abstract class Listener<T extends Event> {
+	abstract onMessage(data: T['data'], msg: Message): void;
+	// subject is subject provided as the argument!
+	abstract subject: T['subject'];
 	abstract queueGroupName: string;
 	private client: Stan;
 	protected ackWait = 5 * 1000;
--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,7 +1,10 @@
 import { Listener } from "./base-listener";
 import { Message } from "node-nats-streaming";
-export class TicketCreatedListener extends Listener {
-	subject = "ticket:created";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+	// provide type annotation so won't change type of subject to anything else
+	subject: Subjects.TicketCreated = Subjects.TicketCreated;
 	queueGroupName = "payments-service";
 	onMessage(data: any, msg: Message) {
 		// handle what need to do from Message
