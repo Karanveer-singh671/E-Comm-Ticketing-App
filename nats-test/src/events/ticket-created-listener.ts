58d4d52e625a838fb416281f17d1eb9cd3fdec52
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-10-02 10:05:42 -0700

    added correct TicketCreatedEvent data for the ticketcreatedlistener

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -4,9 +4,10 @@ import { TicketCreatedEvent } from "./ticket-created-event";
 import { Subjects } from "./subjects";
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
 	// provide type annotation so won't change type of subject to anything else
-	subject: Subjects.TicketCreated = Subjects.TicketCreated;
+	// or use readonly!
+	readonly subject = Subjects.TicketCreated;
 	queueGroupName = "payments-service";
-	onMessage(data: any, msg: Message) {
+	onMessage(data: TicketCreatedEvent["data"], msg: Message) {
 		// handle what need to do from Message
 		console.log("Event Data!", data);
 		// mark message as successful parsed
