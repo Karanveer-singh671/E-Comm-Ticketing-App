833096a51d4a35ee9138c6caa77d4b57c43e738b
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-10-04 09:37:09 -0700

    orders-deployment file as well as orders mongo depl file created and added path to skaffold to see changes and apply the infra files and changed context of auth in the tickets image to tickets since was getting not found before

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@ksticketing/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+// traffic is being proxy'd to our app thru ingress-nginx
+// makes express aware of this and trust this proxy since default is not to trust
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+	cookieSession({
+		signed: false, // no encryption
+		// says when we set equal to test it will be false else we require it to be true
+		// to allow supertest to work since it doesn't send request thru https connection
+		secure: process.env.NODE_ENV !== "test", // Https connection
+	})
+);
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(indexTicketRouter);
+app.use(showTicketRouter);
+app.use(updateTicketRouter);
+app.all("*", async (req, res) => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app }; // named export so use curly brace
