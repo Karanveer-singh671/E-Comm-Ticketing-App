c618367bdb98b0b0ed89526c8d8f3dd888dfb50d
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-10-05 13:08:58 -0700

    ticketupdatedListener

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,25 @@
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, TicketUpdatedEvent } from "@ksticketing/common";
+import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+	readonly subject = Subjects.TicketUpdated;
+	queueGroupName = queueGroupName;
+
+	async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+		const ticket = await Ticket.findById(data.id);
+		const { title, price } = data;
+		if (!ticket) {
+			throw new Error("Ticket not found");
+		}
+
+		ticket.set({
+			title,
+			price,
+		});
+		await ticket.save();
+
+		msg.ack();
+	}
+}
