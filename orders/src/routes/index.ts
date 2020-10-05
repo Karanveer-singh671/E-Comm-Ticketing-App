c5e6feedf4f618a54165f9709f7a5c38155bda0b
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-10-04 19:02:30 -0700

    basic route handler for a user to see all their orders

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from "express";
-
+import { requireAuth } from "@ksticketing/common";
+import { Order } from "../models/order";
 const router = express.Router();
+// don't need validateRequest since get request and not checking a body
+// find all orders that relate to a particular user
+router.get("/api/order", requireAuth, async (req: Request, res: Response) => {
+	const orders = await Order.find({
+		userId: req.currentUser!.id,
+	})
+		// fetch the ticket that is associated with the order
+		.populate("ticket");
 
-router.get("/api/order", async (req: Request, res: Response) => {
-	res.send({});
+	res.send(orders);
 });
 
 export { router as indexOrderRouter };
