12a605c886da4088f702f307f45eb68d4236681e
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-10-05 15:06:55 -0700

    versioning on listener

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,7 +8,12 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
 	queueGroupName = queueGroupName;
 
 	async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
-		const ticket = await Ticket.findById(data.id);
+		const ticket = await Ticket.findOne({
+			// underscore id since in mongo it is saved like this and will only change when we call toJSON
+			_id: data.id,
+			// version for the listener should have the event version - 1 (event should have updated version to update the service's old version)
+			version: data.version - 1,
+		});
 		const { title, price } = data;
 		if (!ticket) {
 			throw new Error("Ticket not found");
