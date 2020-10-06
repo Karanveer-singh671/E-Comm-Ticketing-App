9a0dabfdd782334a575ba4303d3e843ea7147752
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-10-06 15:34:27 -0700

    payment-created-publisher added in payments service

--- payments/src/events/publishers/payment-created-publisher.ts
+++ payments/src/events/publishers/payment-created-publisher.ts
@@ -1,5 +1,5 @@
 import { Subjects, Publisher, PaymentCreatedEvent } from "@ksticketing/common";
 
 export class PaymentCreatedPublisher extends Publisher<PaymentCreatedEvent> {
-	readonly subject = Subjects.PaymentComplete;
+	readonly subject = Subjects.PaymentCreated;
 }
