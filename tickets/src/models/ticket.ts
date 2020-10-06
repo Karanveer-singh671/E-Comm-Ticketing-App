e72342b3c78d8fc97d4a17dfdf85831ad56bb3d1
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-10-05 17:45:35 -0700

    on message for order created listener

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -12,6 +12,7 @@ interface TicketDoc extends mongoose.Document {
 	userId: string;
 	// to add version instead of writing ticket.__v
 	version: number;
+	orderId?: string
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -31,6 +32,9 @@ const ticketSchema = new mongoose.Schema(
 		userId: {
 			type: String,
 			required: true,
+		},
+		orderId: {
+			type: String,
 		},
 	},
 	{
