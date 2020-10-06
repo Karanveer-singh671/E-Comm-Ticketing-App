e5e431429ef11bbb747e664605b98c350c0d2a96
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-10-05 17:52:59 -0700

    setup function for test for order created listener

---
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,37 @@
+import { OrderCreatedListener } from "../order-created-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+import { OrderCreatedEvent, OrderStatus } from "@ksticketing/common";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+	// create an instance of the Listener
+	const listener = new OrderCreatedListener(natsWrapper.client);
+	// create and save the ticket
+	const ticket = Ticket.build({
+		title: "concert",
+		price: 10,
+		userId: "asdf",
+	});
+	await ticket.save();
+
+	// create the fake data event
+	const data: OrderCreatedEvent["data"] = {
+		id: mongoose.Types.ObjectId().toHexString(),
+		version: 0,
+		status: OrderStatus.Created,
+		userId: "asdf",
+		expiresAt: "asdf",
+		ticket: {
+			id: ticket.id,
+			price: ticket.price,
+		},
+	};
+	// @ts-ignore
+	const msg: Message = {
+		ack: jest.fn(),
+	};
+
+	return { listener, ticket, data, msg };
+};
