d28473a1bab71972ec3aed8ed63dd3e87bdc3934
karanveer singh <karanveersingh@karanveers-MacBook-Pro.local>
2020-10-07 09:01:32 -0700

    landing page update

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,34 @@
 const LandingPage = ({ currentUser }) => {
-	return currentUser ? (
-		<h1>You are signed in</h1>
-	) : (
-		<h1>You are NOT signed in</h1>
+	// loop over array of tickets and build the table record with the row value as title and price
+	const ticketList = tickets.map((ticket) => {
+		return (
+			<tr key={ticket.id}>
+				<td>{ticket.title}</td>
+				<td>{ticket.price}</td>
+			</tr>
+		);
+	});
+
+	return (
+		<div>
+			<h1>Tickets</h1>
+			<table className="table">
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>{TicketList}</tbody>
+			</table>
+		</div>
 	);
 };
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-	return {};
+	const { data } = await client.get("api/tickets");
+	// will be merged as props in component of LandingPage
+	return { tickets: data };
 };
 
 export default LandingPage;
